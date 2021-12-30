55d6dffd1f06908db611aa74a00d9788c5f95fcb
AS <aurelien.sudre@gmail.com>
2021-12-30 18:33:31 +0100

    SpriteSheetData

--- client/src/models/Level.ts
+++ client/src/models/Level.ts
@@ -1,13 +1,24 @@
 import DrawHelper from "../helpers/DrawHelper.js";
+import SpriteSheet from "./base/SpriteSheet.js";
 import LevelData from "./LevelData.js";
 
 export default class Level implements GameObject {
   public name: string;
   public tag: string;
-
   public width: number;
   public height: number;
   public color: string;
+
+  private levelData: LevelData;
+  private tileWidth: number;
+  private tileHeight: number;
+  private columnCount: number;
+  private rowCount: number;
+  private backgroundData: number[];
+  private foregroundData: number[];
+  private breakableData: number[];
+  private levelSpriteSheet: SpriteSheet;
+  private spriteSheetData: SpriteSheetData;
 
   public constructor(
     name: string,
@@ -16,34 +27,40 @@ export default class Level implements GameObject {
     color: string
   ) {
     this.name = name;
-
     this.width = width;
     this.height = height;
-
     this.color = color;
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
   public async initialize(): Promise<void> {
     const response = await window.fetch(`/levels/${this.name}.json`);
-    const levelData = await (response.json() as Promise<LevelData>);
+    this.levelData = await (response.json() as Promise<LevelData>);
+
+    this.tileWidth = this.levelData.tilewidth;
+    this.tileHeight = this.levelData.tileheight;
+    this.columnCount = this.levelData.width;
+    this.rowCount = this.levelData.height;
+
+    this.backgroundData = this.levelData.layers.find(
+      (l) => l.name === "background"
+    ).data;
+    this.foregroundData = this.levelData.layers.find(
+      (l) => l.name === "foreground"
+    ).data;
+    this.breakableData = this.levelData.layers.find(
+      (l) => l.name === "breakable"
+    ).data;
+
+    const spriteSheet = this.levelData.tilesets[0].source.replace("../", "");
+
+    const response2 = await window.fetch(spriteSheet);
+    this.spriteSheetData = await (response2.json() as Promise<SpriteSheetData>);
+
+    this.levelSpriteSheet = new SpriteSheet(spriteSheet, 2, 4);
   }
 
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   public async update(deltaTime: number, totalTime: number): Promise<void> {}
 
-  public async draw(context: CanvasRenderingContext2D): Promise<void> {
-    DrawHelper.drawRectangle(
-      context,
-      0,
-      0,
-      this.width,
-      this.height,
-      this.color,
-      1,
-      this.color,
-      false,
-      true
-    );
-  }
+  public async draw(context: CanvasRenderingContext2D): Promise<void> {}
 }
---
+++ client/src/models/SpriteSheetData.ts
@@ -0,0 +1,16 @@
+export default class SpriteSheetData {
+  public columns: number;
+  public image: string;
+  public imageheight: number;
+  public imagewidth: number;
+  public margin: number;
+  public name: string;
+  public spacing: number;
+  public tilecount: number;
+  public tiledversion: string;
+  public tileheight: number;
+  public tilewidth: number;
+  public transparentcolor: string;
+  public type: string;
+  public version: string;
+}
